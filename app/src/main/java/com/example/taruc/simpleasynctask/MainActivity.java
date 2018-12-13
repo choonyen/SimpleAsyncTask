8f94efc1548ecb6df574e2fb19571b0c094b4b68
anreh <anurehka@gmail.com>
2018-12-13 12:57:23 +0800

    Initial commit

---
+++ app/src/main/java/com/example/taruc/simpleasynctask/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.taruc.simpleasynctask;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextView;
+    private static final String TEXT_STATE ="currentText";
+
+    protected void onSaveInstanceState(Bundle outState)
+    {
+        super.onSaveInstanceState(outState);
+        outState.putString(TEXT_STATE, mTextView.getText().toString());
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if(savedInstanceState!=null)
+        {
+            mTextView.setText(savedInstanceState.getString(TEXT_STATE));
+        }
+    }
+
+    public void startTask(View view)
+    {
+        mTextView =findViewById(R.id.textView1);
+        mTextView.setText(R.string.napping);
+        new SimpleAsyncTask(mTextView).execute();
+    }
+}
